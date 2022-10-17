ba0c800c64c6e48512f3430af1a589a7f4521158
Kristi <112578194+kvanme01@users.noreply.github.com>
2022-10-17 20:08:05 +0000

    adding to main

--- main.sh
+++ main.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
+case $1 in 
+    
+    # Show the help message
+    -h | --help)
+    echo "Options:"
+    echo "  -h, --help: Show this help message"
+    echo "  run_scrape: Scrape the data from the website"
+    echo "  print_scrape: Scrape the data and print it to the terminal"
+    echo "  keyword_search: Run a keyword search on user input"
+    ;;
+
+    # Run get_data function
+    run_scrape)
+    python -c'import common.py; common.get_data()'
+    
+    ;;
+
+    # Run print_df function
+    print_scrape)
+    python -c'import common.py; common.print_df()'
+
+    ;;
+
+    # Run run_search function
+    keyword_search)
+    echo "Enter a keyword to search for: "
+    read keyword
+    python -c'import common.py; common.run_search(keyword)'
+
+
+    ;;
+esac
+
 
