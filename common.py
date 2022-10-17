b86f596adea523d9d08719f69b872784e91070f1
Kristi <112578194+kvanme01@users.noreply.github.com>
2022-10-17 19:15:50 +0000

    functions

--- common.py
+++ common.py
@@ -1,36 +1,48 @@
-import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+# scrapes site for data, returns df
 def get_data():
-    website = 'https://www.journals.elsevier.com/journal-of-advanced-research/most-downloaded-articles'
+    website = "https://www.journals.elsevier.com/journal-of-advanced-research/most-downloaded-articles"
     website_url = requests.get(website).text
-    soup = BeautifulSoup(website_url,'html.parser')
+    soup = BeautifulSoup(website_url, "html.parser")
 
     # make a list of titles
     titles = []
-    for title in soup.find('ul', class_= 'sc-9zxyh7-0 ffmPq'):
+    for title in soup.find("ul", class_="sc-9zxyh7-0 ffmPq"):
         titles.append(title.get_text())
 
     # make a list of hyperlinks
     hyper = []
-    for link in soup.find('ul', class_= 'sc-9zxyh7-0 ffmPq').find_all('a', href=True):
-        hyper.append(link['href'])
+    for link in soup.find("ul", class_="sc-9zxyh7-0 ffmPq").find_all("a", href=True):
+        hyper.append(link["href"])
 
     # create a dataframe from the lists
-    df = pd.DataFrame({'Title': titles, 'Hyperlink': hyper})
+    df = pd.DataFrame({"Title": titles, "Hyperlink": hyper})
     return df
+
 
 # searches df for keyword input by user in CL, returns list with any matching values
 def search_df(df, keyword):
-    for title in 
-
-
+    search = df[df["Title"].str.contains(keyword, case=False)]
+    if df["Title"].str.contains(keyword, case=False).any():
+        return search
 
 
 def run_search(keyword):
     df = get_data()
-    value = search_df(df, keyword)
+    results = search_df(df, keyword)
+    print(results)
 
 
+def print_df():
+    df = get_data()
+    print(df)
+
+
+# def test():
+#    run_search("plant")
+
+
+# test()
